4aa7ef9f54ae74efde024bb10a6047a401a1a7ff
mariashka <bisyarinamariashka@gmail.com>
2016-12-26 06:49:03 +0300

    Finished hw11

--- hw11/src/Main.hs
+++ hw11/src/Main.hs
@@ -1,29 +1,35 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE StrictData #-}
+{-# LANGUAGE Strict            #-}
+{-# LANGUAGE TemplateHaskell   #-}
 {-# OPTIONS -Wall #-}
 module Main where
 
-import           Data.Map (fromList, fold, (!), insert, Map)
-import qualified System.Random as R
-import qualified Control.Monad.Random as R
-import qualified Control.Concurrent.MVar as MVar
-import qualified Control.Concurrent as C
-import           Control.Parallel.Strategies(runEval, rpar)
-import           Control.Monad(replicateM)
-import           Data.Foldable(traverse_)
-import           Control.Lens.TH(makeLenses)
-import           Control.Lens ((%~), (&), (.~), (^.), _1)
+
+import qualified Control.Concurrent               as C
+import qualified Control.Concurrent.MVar          as MVar
 import qualified Control.Concurrent.ThreadManager as TM
+import           Control.Exception                (bracket_)
+import           Control.Lens                     ((%~), (&), (.~), (^.), _1)
+import           Control.Lens.TH                  (makeLenses)
+import           Control.Monad                    (forM_, when)
+import qualified Control.Monad.Random             as R
+import           Control.Parallel.Strategies      (rpar, runEval)
+import qualified Data.Text                        as T (pack)
+import           Data.Vector                      (Vector, foldr', fromList, toList, (!),
+                                                   (//))
+import           System.Wlog                      (initLoggingFromYaml, logError, logInfo,
+                                                   logNotice, releaseAllHandlers,
+                                                   usingLoggerName)
 
 type MagicGenerator = R.StdGen
-data Magician = Magician { _attack :: Int
-                         , _shield :: Int
-                         , _speed :: Int
+data Magician = Magician { _attack      :: Int
+                         , _shield      :: Int
+                         , _speed       :: Int
                          , _instability :: Int
-                         , _idx :: Int
-                         , _gens :: [MagicGenerator]
+                         , _idx         :: Int
+                         , _gens        :: [MagicGenerator]
                          }
+  deriving Show
 makeLenses ''Magician
 
 genParameters :: (R.RandomGen g) => R.Rand g (Int -> [MagicGenerator] -> Magician)
@@ -42,46 +48,72 @@ pmap _ [] = []
 pmap f (x:xs) = runEval $ do
   x' <- rpar (f x)
   xs' <- rpar (pmap f xs)
-  return $ (x':xs')
+  return (x':xs')
 
-getInstableRandom :: Int -> Int -> MagicGenerator -> (Int, MagicGenerator)
-getInstableRandom up times g = (R.runRand (replicateM times $ R.getRandomR (1, up)) g) & _1 %~ head
+getInstable :: Int -> Int -> MagicGenerator -> (Int, MagicGenerator)
+getInstable up 1 g = R.randomR (1, up) g
+getInstable up times g = getInstable up (times - 1) r
+  where (_, r) = R.randomR (1, up) g
+
+getTarget :: Int -> Int -> MagicGenerator -> (Int, MagicGenerator)
+getTarget n i g = R.runRand (R.getRandomR (0, n - 2)) g & _1 %~ process
+  where process target = if target < i then target else target + 1
 
 attackGen :: Int -> Magician -> (Int, Int, Magician)
-attackGen n m = (maximum $ map fst result, target, m & gens .~ newGens')
-  where result = pmap (getInstableRandom (m^.attack) (m^.instability)) (m^.gens)
-        newGens = map snd result
-        (target, newHead) = R.runRand (R.getRandomR (1, n)) $ head newGens
-        newGens' = newHead:(tail newGens)
+attackGen n m = (maximum $ map fst result, t, m & gens .~ g:tail (map snd result))
+  where result = pmap (getInstable (m^.attack) (m^.instability)) $ m^.gens
+        (t, g) = getTarget n (m^.idx) $ snd $ head result
 
-applyAttack :: Int -> Int -> Int -> Map Int Magician -> IO (Map Int Magician, Maybe Magician)
-applyAttack mid attackLvl tid stats = do
-  let enemy = stats ! tid
-      me    = stats ! mid
-      count = fold (\x res -> if x^.shield > 0 then res + 1 else res) (0::Int) stats
-  if count == 1 then return (stats, Nothing) else do
-    if me^.shield == 0 || (enemy^.shield) == 0 then return (stats, Just me) else do
-      if enemy^.shield <= attackLvl then do
-        let newMe = me & gens %~ (++ enemy^.gens)
-        return (insert tid (enemy & shield .~ 0) $ insert mid newMe stats, Just newMe)
-      else
-        return (insert tid (enemy & shield %~ (\x -> x - attackLvl)) stats, Just me)
+applyAttack :: Magician -> Int -> Int -> Vector Magician -> IO (Vector Magician, Maybe Magician)
+applyAttack m attackLvl tid stats
+  | me^.shield == 0 = do
+      usingLoggerName "fight" $ logError $ T.pack $ "Someone already defeated attacker: " ++ "magician" ++ show mid
+      return (stats // [(mid, me)], Nothing)
+  | count == 1 = do
+    usingLoggerName "fight" $ logError $ T.pack $ "magician" ++ show mid ++ " is the last one"
+    return (stats, Nothing)
+  | enemy^.shield == 0 = do
+      usingLoggerName "fight" $ logError $ T.pack $ "Enemy " ++ "magician" ++ show tid ++ " is already defeated"
+      return (stats // [(mid, me)], Just me)
+  | enemy^.shield <= attackLvl = do
+      let newMe = me & gens %~ (++ enemy^.gens)
+      usingLoggerName "fight" $ logNotice $ T.pack $ "magician" ++ show tid ++ " is defeated by " ++ "magician" ++ show mid
+      return (stats // [(tid, enemy & shield .~ 0), (mid, newMe)], Just newMe)
+  | otherwise = do
+      usingLoggerName "fight" $ logNotice $ T.pack $ "magician" ++ show tid ++ " is hit by " ++ "magician" ++ show mid ++ " with " ++ show attackLvl
+      return (stats // [(tid, enemy & shield %~ (\x -> x - attackLvl)), (mid, me)], Just me)
+  where enemy = stats ! tid
+        mid   = m^.idx
+        me = (stats ! mid) & gens .~ (m^.gens)
+        count = foldr' (\x res -> if x^.shield > 0 then res + 1 else res) (0::Int) stats
 
-fight :: Int -> MVar.MVar (Map Int Magician) -> Magician -> IO ()
+fight :: Int -> MVar.MVar (Vector Magician) -> Magician -> IO ()
 fight n stats magician = do
   let (attackLvl, target, m) = attackGen n magician
-  C.threadDelay $ 1000 * m^.instability
-  updMag <- MVar.modifyMVar stats (applyAttack (m^.idx) attackLvl target)
-  case updMag of
-    Nothing -> return ()
-    Just mag -> if mag^.shield > 0 then fight n stats mag else return ()
+  usingLoggerName "fight" $ do
+    logInfo $ T.pack $ "magician" ++ show (m^.idx) ++ " generated attack info: attack force " ++ show attackLvl ++ " target: " ++ show target
+    logInfo $ T.pack $ "magician" ++ show (m^.idx) ++ " waiting for " ++ show (m^.speed) ++ " seconds"
+  C.threadDelay $ 1000000 * m^.speed
+  usingLoggerName "fight" $ logInfo $ T.pack $ "magician" ++ show (m^.idx) ++ " is going to attack " ++ "magician" ++ show target
+  Just x <- MVar.modifyMVar stats (applyAttack m attackLvl target)
+  when (x^.shield > 0) $ fight n stats x
 
-main :: IO ()
-main = do
+runThreads :: IO ()
+runThreads = do
   line <- getLine
   let n = read line
-  magicians <- traverse createMagician [1 .. n - 1]
-  stats <- MVar.newMVar $ fromList $ zip [1 .. n - 1] magicians
+  magicians <- traverse createMagician [0 .. n - 1]
+  stats <- MVar.newEmptyMVar
+  MVar.putMVar stats $ fromList magicians
   manager <- TM.make
-  traverse_ ((TM.fork manager) . fight n stats) magicians
+  forM_ magicians $ \m -> (TM.fork manager . fight n stats) m
   TM.waitForAll manager
+  z <- MVar.readMVar stats
+  let winner = head $ filter (\x -> x^.shield /= 0) (toList z)
+  putStrLn $ "Winner " ++ show (winner^.idx) ++ " shield: " ++ show (winner^.shield) ++ ", amount of generators: "++ show (length (winner^.gens))
+
+main :: IO ()
+main = bracket_
+  (initLoggingFromYaml "logger-config.yaml" $ Just "logs")
+  releaseAllHandlers
+  runThreads
